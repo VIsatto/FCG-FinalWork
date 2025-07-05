05d0f2a8f6bfec5e26910a06a657bc0e5b1d3e67
Eduardo Veiga <eduardo85011162@gmail.com>
2025-07-05 16:28:06 -0300

    Fixed bug animation

--- src/main.cpp
+++ src/main.cpp
@@ -391,6 +391,8 @@ int main(int argc, char* argv[])
         animateObject(&sonic_position, camera_view_vector, speed, delta_t);
         WallsCollision(&sonic_position);
 
+        camera_position_c  = sonic_position + glm::vec4(x,y,z,0.0f); 
+
         // Computamos a matriz "View" utilizando os parâmetros da câmera para
         // definir o sistema de coordenadas da câmera.  Veja slides 2-14, 184-190 e 236-242 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
         glm::mat4 view = Matrix_Camera_View(camera_position_c, camera_view_vector, camera_up_vector);
