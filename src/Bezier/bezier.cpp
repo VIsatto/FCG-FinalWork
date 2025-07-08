537d39f14f9d4ba659de41c986b7b6c00a59fe38
VIsatto <vicente.isatto@gmail.com>
2025-07-08 12:39:05 -0300

    Applied the polynomial formula
    It's way easier

--- src/Bezier/bezier.cpp
+++ src/Bezier/bezier.cpp
@@ -1,23 +1,23 @@
 #include "bezier.h"
 
 
-void BezierPoint(std::vector<glm::vec4> &curve_points, float t, glm::vec4 p1, glm::vec4 p2) {
-    // Calcula a posição do ponto na curva de Bézier cúbica
-    glm::vec4 point = p1 + (p2 - p1) * t;
-    curve_points.push_back(point);
-}
+// void BezierPoint(std::vector<glm::vec4> &curve_points, float t, glm::vec4 p1, glm::vec4 p2) {
+//     // Calcula a posição do ponto na curva de Bézier cúbica
+//     glm::vec4 point = p1 + (p2 - p1) * t;
+//     curve_points.push_back(point);
+// }
 
-std::vector<glm::vec4> BezierLoop(std::vector<glm::vec4> ctrl_points, int degree, float t) {
-    std::vector<glm::vec4> curve_points;
-    for (int i = 0; i < degree - 1; i++) {
-        glm::vec4 p1 = ctrl_points[i];
-        glm::vec4 p2 = ctrl_points[i + 1];
-        BezierPoint(curve_points, t, p1, p2);
-    }
-    if(degree > 0)
-        curve_points = BezierLoop(curve_points, degree - 1, t);
-    return curve_points;
-}
+// std::vector<glm::vec4> BezierLoop(std::vector<glm::vec4> ctrl_points, int degree, float t) {
+//     std::vector<glm::vec4> curve_points;
+//     for (int i = 0; i < degree - 1; i++) {
+//         glm::vec4 p1 = ctrl_points[i];
+//         glm::vec4 p2 = ctrl_points[i + 1];
+//         BezierPoint(curve_points, t, p1, p2);
+//     }
+//     if(degree > 0)
+//         curve_points = BezierLoop(curve_points, degree - 1, t);
+//     return curve_points;
+// }
 
 std::vector<glm::vec4> BezierCurve(std::vector<glm::vec4> ctrl_points) {
     int values = 100;
@@ -37,8 +37,10 @@ std::vector<glm::vec4> BezierCurve(std::vector<glm::vec4> ctrl_points) {
     }
 
     for (float t : t_values) {
-        std::vector<glm::vec4> segment_points = BezierLoop(ctrl_points, ctrl_points.size() - 1, t);
-        point = segment_points[0]; // Pega o primeiro ponto do segmento
+        point = static_cast<float>(std::pow(1 - t, 3)) * ctrl_points[0] + 
+                static_cast<float>(3 * t * std::pow(1 - t, 2)) * ctrl_points[1] + 
+                static_cast<float>(3 * std::pow(t, 2) * (1 - t)) * ctrl_points[2] + 
+                static_cast<float>(std::pow(t, 3)) * ctrl_points[3];
         curve_points.push_back(point);
     }
     return curve_points;
